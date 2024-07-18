a0b6b2296c98f0f7cbfa8c62f3c93af6c50bfd6b
parham <parham.mehrabi.webdev@gmail.com>
2024-07-18 15:24:26 +0330

    move matrices shape selection into a single frame create one line for both

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from tkinter import Tk, OptionMenu, StringVar, Button, Checkbutton, BooleanVar
+from tkinter import Tk, OptionMenu, StringVar, Button, Checkbutton, BooleanVar, LEFT, Frame
 from colors import BACK_GROUND_COLOR_1
-from widgets import BlueLabel
+from widgets import BlueLabel, matrices_frame
 from utils import matrices
+
+
 
 
 def main():
@@ -10,23 +12,17 @@ def main():
     root.geometry("800x620+100+100")
     root.config({"bg": BACK_GROUND_COLOR_1})
     BlueLabel(root, text="select difficulty").pack()
-    matrix_1 = StringVar()
-    matrix_1.set("select the first matrix")
-    matrix_num1 = OptionMenu(root, matrix_1, *matrices)
-    matrix_2 = StringVar()
-    matrix_2.set("select the second matrix")
-    matrix_num2 = OptionMenu(root, matrix_2, *matrices)
     have_zero_var = BooleanVar()
     have_zero_var.set(True)
     btn_have_zero = Checkbutton(root, text="allow zero", variable=have_zero_var)
     have_negative_var = BooleanVar()
     have_negative_var.set(True)
     btn_negative = Checkbutton(root, text="allow negative items", variable=have_negative_var)
-
     btn_generate = Button(root, text="generate")
+
+    _matrices_frame = matrices_frame(root)
     
-    matrix_num1.pack(pady=2)
-    matrix_num2.pack(pady=2)
+    _matrices_frame.pack(pady=2)
     btn_have_zero.pack(pady=2)
     btn_negative.pack(pady=2)
     btn_generate.pack(pady=2)
--- widgets.py
+++ widgets.py
@@ -1,6 +1,7 @@
 """ my costume widgets """
-from tkinter import Label
-from colors import BACK_GROUND_COLOR_1, CYAN_1
+from tkinter import Label, Frame, StringVar, OptionMenu, LEFT, Tk
+from utils import matrices
+from colors import BACK_GROUND_COLOR_1, CYAN_1, BLACK, RED
 
 
 class BlueLabel(Label):
@@ -9,3 +10,27 @@ class BlueLabel(Label):
         kwargs.setdefault("fg", CYAN_1)
         kwargs.setdefault("font", ("MonoSpace", 18, 'bold'))
         super().__init__(master, **kwargs)
+
+class XSgin(Label):
+    def __init__(self, master, **kwargs):
+        kwargs.setdefault("bg", BACK_GROUND_COLOR_1)
+        kwargs.setdefault("text", "X")
+        kwargs.setdefault("fg", BLACK)
+        kwargs.setdefault("font", ("Aryal", 25, 'bold'))
+        super().__init__(master, **kwargs)
+
+    
+
+def matrices_frame(master:Tk) -> Frame:
+    _matrices_frame = Frame(master)
+    matrix_1 = StringVar()
+    matrix_1.set("select the first matrix")
+    matrix_num1 = OptionMenu(_matrices_frame, matrix_1, *matrices)
+    x_sign = XSgin(_matrices_frame, fg=RED)
+    matrix_2 = StringVar()
+    matrix_2.set("select the second matrix")
+    matrix_num2 = OptionMenu(_matrices_frame, matrix_2, *matrices)
+    matrix_num1.pack(side=LEFT)
+    x_sign.pack(side=LEFT)
+    matrix_num2.pack(side=LEFT)
+    return _matrices_frame
