60649949931c73af95447399030c9cdfd6d99edf
parham <parham.mehrabi.webdev@gmail.com>
2024-07-18 15:08:41 +0330

    fix Blue Label
    fix the color and increase the font size and bold the text

--- widgets.py
+++ widgets.py
@@ -1,9 +1,11 @@
 """ my costume widgets """
 from tkinter import Label
-from colors import BACK_GROUND_COLOR_1
+from colors import BACK_GROUND_COLOR_1, CYAN_1
 
 
 class BlueLabel(Label):
     def __init__(self, master, **kwargs):
         kwargs.setdefault("bg", BACK_GROUND_COLOR_1)
+        kwargs.setdefault("fg", CYAN_1)
+        kwargs.setdefault("font", ("MonoSpace", 18, 'bold'))
         super().__init__(master, **kwargs)
